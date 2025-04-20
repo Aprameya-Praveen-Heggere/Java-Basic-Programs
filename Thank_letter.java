0d9977581850612227d975c16374a8bd064a458e
Aprameya-Praveen-Heggere <hpaprameya1@gmail.com>
2025-04-20 13:47:22 +0530

    Thank you letter
    This program has a default format to thank people.
    You can add name while executing the program itself, whereas, the template can be changed according to custom needs by replacing the default message in the code by custom message.

---
+++ Thank_letter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Thank_letter
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the name of the person to be thanked");
+        String name=sc.nextLine();
+        System.out.println("Thank you very much "+name+", your help has been very valuable for me");
+    }
+}
